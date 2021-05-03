e40954d660f6fc4b8f56e5bee409b7c201f35ede
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2021-05-03 16:31:48 -0300

    Add start of begining home view

--- resources/js/views/categories.vue
+++ resources/js/views/categories.vue
@@ -1,3 +1,35 @@
 <template>
-    <p>Hello World</p>
+    <Main>
+        <header><h1 class="text-6xl font-extrabold m-3">Choose your product category:</h1></header>
+        <section class="grid grid-cols-1 md:grid-cols-3 gap-4 absolute w-15">
+            <div v-for="category in categories"
+                 class="bg-primary text-secondary font-extrabold text-lg text-md-center
+                 p-4 text-center grid grid-cols-1
+                 rounded-lg transform transition duration-500 hover:scale-110">
+                <img :src=getCategoryImage(category) alt="Category image" width="100px">
+                <hr class="mt-3" />
+                {{ category.name }}
+            </div>
+        </section>
+    </Main>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            categories: []
+        }
+    },
+
+    mounted() {
+        axios.get('/categories').then(({data}) => this.categories = data)
+    },
+    methods: {
+        getCategoryImage(category) {
+            return `${category.image}`;
+        }
+    }
+
+}
+</script>
