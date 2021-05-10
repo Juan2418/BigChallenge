c12a0725f72fdee6ff226901530cfb55e37581fe
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2021-05-10 15:18:59 -0300

    Add cart view

---
+++ resources/js/views/Cart.vue
@@ -0,0 +1,26 @@
+<template>
+    <Main class="grid grid-cols-2 grid-cols-main bg-gray-200">
+        <nav-bar></nav-bar>
+        <section class="container h-screen overflow-y-scroll">
+            <p class="font-bold text-2xl my-4">Your products</p>
+            <product-list-horizontal :products="products"/>
+        </section>
+    </Main>
+</template>
+
+<script>
+import NavBar from "../components/NavBar";
+import ProductListHorizontal from "../components/ProductListHorizontal";
+
+export default {
+    name: "cart",
+    components: {ProductListHorizontal, NavBar},
+
+    data() {
+        return {
+            products: Store.productsToOrder
+        }
+    }
+}
+</script>
+
