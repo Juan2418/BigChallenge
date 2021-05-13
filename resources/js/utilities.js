61369e23aa5f98605527915b1b608dada3c1bf0e
Juan Pablo Rodriguez Sotto <juan.rodriguezsotto@gmail.com>
2021-05-13 16:17:09 -0300

    Add Credit card validation

--- resources/js/utilities.js
+++ resources/js/utilities.js
@@ -3,11 +3,32 @@ export const goToHome = () => {
 }
 export const modifyProduct = (product) => {
     Store.productToModify = product;
-    SPA.$router.push('/');
+    SPA.$router.push('/modify');
 }
 
 export const sendOrder = (products) => {
-    axios.post('api/order', products);
+    axios.post('api/order', {'products': products});
+}
+
+export const creditCardIsValid = (value) => {
+    // Accept only digits, dashes or spaces
+    if (/[^0-9-\s]+/.test(value)) return false;
+
+    //Luhn Algorithm
+    let nCheck = 0, bEven = false;
+    value = value.replace(/\D/g, "");
+
+    for (let n = value.length - 1; n >= 0; n--) {
+        let cDigit = value.charAt(n),
+            nDigit = parseInt(cDigit, 10);
+
+        if (bEven && (nDigit *= 2) > 9) nDigit -= 9;
+
+        nCheck += nDigit;
+        bEven = !bEven;
+    }
+
+    return (nCheck % 10) == 0;
 }
 
 export default {
--- resources/js/views/CreditCardPayment.vue
+++ resources/js/views/CreditCardPayment.vue
@@ -2,16 +2,44 @@
     <Main class="grid grid-cols-2 grid-cols-main bg-gray-200">
         <nav-bar></nav-bar>
         <section class="container h-screen overflow-y-scroll">
-            
+            <section class="flex flex-col py-60">
+                <input type="text" @click="error = false" v-model="creditCardNumber" class="text-xl h-10" maxlength="20">
+                <p v-if="error" class="text-red-900 text-sm py-2">
+                    *Invalid credit card.
+                </p>
+                <button @click="payCreditCard" class="btn btn-primary py-4">Pay</button>
+            </section>
+
         </section>
+
     </Main>
 </template>
 
 <script>
 import NavBar from "../components/NavBar";
+import {creditCardIsValid} from '../utilities.js';
+
 export default {
     name: "creditcard-payment",
-    components: {NavBar}
+    components: {NavBar},
+    data() {
+        return {
+            creditCardNumber: "",
+            error: false
+        }
+    },
+    methods: {
+        creditCardIsValid,
+        payCreditCard() {
+            if (!creditCardIsValid(this.creditCardNumber) || this.creditCardNumber.length === 0) {
+                this.error = true;
+                return;
+            }
+            this.sendOrder(Store.productsToOrder);
+            Store.productsToOrder = [];
+            this.goToHome();
+        }
+    }
 }
 </script>
 
